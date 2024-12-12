4a4de13a64adb9972666160a10d6497a12e55128
Vikas Gautam <v33vikasgautam@gmail.com>
2024-12-13 02:19:45 +0530

    solved Day 11 2024

---
+++ 2024/11.lua
@@ -0,0 +1,72 @@
+#!/usr/bin/env lua
+
+function ParseInput()
+  local nums = {}
+  while true do
+    local a = io.read("*n")
+    if a == nil then
+      break
+    end
+    nums[tostring(a)] = 1
+  end
+  return nums
+end
+
+function table.deep_copy(original)
+  local copy = {}
+  for k, v in pairs(original) do
+    if type(v) == "table" then
+      copy[k] = table.deep_copy(v)
+    else
+      copy[k] = v
+    end
+  end
+  return copy
+end
+
+function ApplyRule(num)
+  if num == "0" then
+    return "1"
+  end
+  local digits = #num
+  if digits % 2 == 0 then
+    local first_half = string.sub(num, 1, digits/2)
+    local second_half = tostring(tonumber(string.sub(num, (digits/2)+1)))
+    return first_half, second_half
+  else
+    return tostring(tonumber(num)*2024)
+  end
+end
+
+function Blink(nums, times)
+  local ping, pong = table.deep_copy(nums), {}
+  for _ = 1, times do
+    for num, cnt in pairs(ping) do
+      local a, b = ApplyRule(num)
+      pong[a] = (pong[a] or 0) + cnt
+      if b ~= nil then
+        pong[b] = (pong[b] or 0) + cnt
+      end
+      ping[num] = nil
+    end
+    -- ping will be empty so pong will use it 
+    -- for next iteration to avoid memory allocation
+    ping, pong = pong, ping
+  end
+  local sum = 0
+  for _, cnt in pairs(ping) do
+    sum = sum + cnt
+  end
+  return sum
+end
+
+function Main()
+  local nums = ParseInput()
+  print("Part1:", Blink(nums, 25))
+  print("Part2:", Blink(nums, 75))
+end
+
+-- time cat 2024/input/11.txt | ./2024/11.lua
+-- Part1:  198075
+-- Part2:  235571309320764
+Main()
